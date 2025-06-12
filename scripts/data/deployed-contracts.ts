90deb8ca0bec03109a1c0754a0123bccffa84792
kiv1n <aloha@kiv1n.ru>
2025-06-12 08:37:30 +0300

    Add questionManagerImpl to deployed contracts for luksoTestnet and luksoMainnet

--- scripts/data/deployed-contracts.ts
+++ scripts/data/deployed-contracts.ts
@@ -4,14 +4,17 @@ export const CONTRACTS: {
   [key: string]: {
     question: Address | undefined;
     questionManager: Address | undefined;
+    questionManagerImpl: Address | undefined;
   };
 } = {
   luksoTestnet: {
     question: "0xA958c128203d2671636f8820E943640A097e6A60",
     questionManager: "0xEDe254159220fEE61bC83FDa5A9f1EA5b510472e",
+    questionManagerImpl: "0x7e303efc29e24e5CD8dC619976C0599e4a17E46e",
   },
   luksoMainnet: {
     question: undefined,
     questionManager: undefined,
+    questionManagerImpl: undefined,
   },
 };
